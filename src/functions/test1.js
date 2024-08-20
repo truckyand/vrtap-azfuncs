5e5290746cc7dbc1ff579fc3ba67b1ffbf7aeb25
Truc Bui <trucbui@appvity.com>
2024-08-20 13:31:24 +0700

    1

---
+++ src/functions/test1.js
@@ -0,0 +1,43 @@
+
+app.http('test1', {
+    methods: ['GET'],
+    authLevel: 'anonymous',
+    handler: async (request, context) => {
+        context.log(`Http function processed request for url "${request.url}". Invocation ID: "${context.invocationId}`);
+
+        //
+        
+
+        //
+        const name = request.query.get('name') || (await request.text()) || 'world';
+        return { body: `Hello, ${name}!` };
+    },
+});
+app.http('test2', {
+    methods: ['GET'],
+    authLevel: 'anonymous',
+    handler: async (request, context) => {
+        context.log(`Http function processed request for url "${request.url}". Invocation ID: "${context.invocationId}`);
+
+        //
+        
+
+        //
+        const name = request.query.get('name') || (await request.text()) || 'world';
+        return { body: `Hello, ${name}!` };
+    },
+});
+app.http('test3', {
+    methods: ['GET'],
+    authLevel: 'anonymous',
+    handler: async (request, context) => {
+        context.log(`Http function processed request for url "${request.url}". Invocation ID: "${context.invocationId}`);
+
+        //
+        
+
+        //
+        const name = request.query.get('name') || (await request.text()) || 'world';
+        return { body: `Hello, ${name}!` };
+    },
+});
