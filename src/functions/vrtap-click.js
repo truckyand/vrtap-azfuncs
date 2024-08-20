20856070ab66b841aa25b6067ecea8c16c8f436a
Truc Bui <trucbui@appvity.com>
2024-08-20 08:56:56 +0700

    update

--- src/functions/vrtap-click.js
+++ src/functions/vrtap-click.js
@@ -1,6 +1,6 @@
 const { app, output } = require('@azure/functions');
 
-app.timer('timerTrigger1', {
+app.timer('vrtap-click-abc', {
     schedule: '0 */5 * * * *',
     handler: (myTimer, context) => {
         context.log(`Something has happened. Invocation ID: "${context.invocationId}"`);
