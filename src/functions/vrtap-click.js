9d5310541e3564042d5e81f382e59619d4ccecf9
Truc Bui <trucbui@appvity.com>
2024-08-20 09:05:43 +0700

    vrtap-click-abc

--- src/functions/vrtap-click.js
+++ src/functions/vrtap-click.js
@@ -1,9 +1,37 @@
 const { app, output } = require('@azure/functions');
+const axios = require('axios').default;
 
+// Config
+//
+const MIN = 8;
+const MAX = 30;
+
+const userId = "ffe70c88-5ca3-4e9a-8b0e-e0f77f6ea5d2";
+const url = `https://api.victoriavr.com/api/v1/user/${userId}/click`;
+
+
+//
 app.timer('vrtap-click-abc', {
     schedule: '0 */5 * * * *',
-    handler: (myTimer, context) => {
-        context.log(`Something has happened. Invocation ID: "${context.invocationId}"`);
-        return { hello: 'world' }
+    handler: async (myTimer, context) => {
+        context.log(`Enter vrtap-click-abc. Invocation ID: "${context.invocationId}"`);
+        //return { hello: 'world' }
+
+        await click(myTimer, context);
     }
 });
+
+//
+async function click(myTimer, context) {
+    try {
+        var click_data = {
+            clickCount: Math.round(Math.random() * (MAX - MIN) + MIN),
+            timestamp: Date.now()
+        };
+        var resp = await axios.post(url, click_data);
+        context.log(`${resp.statusText} / click: ${click_data.clickCount} / energy: ${resp.data.energy} / balance: ${resp.data.balance} / earned: ${resp.data.earned}`);
+    }
+    catch (e) {
+        context.log(e);
+    }
+}
